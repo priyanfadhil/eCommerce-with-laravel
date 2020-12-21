aa21b88997d2f6fb43af9f783953a6dba4da61b2
priyanfadhil <priyanfadhil@gmail.com>
2020-12-21 22:52:47 +0700

    Product Detail Selesai

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -43,5 +43,8 @@
     .trending-wrapper{
         margin: 30px;
     }
+    .detail-img{
+        height: 200px;
+    }
 </style>
 </html>
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -13,11 +13,13 @@
             <div class="carousel-inner">
                 @foreach($products as $item)
                     <div class="item {{$item['id']==1?'active':''}}">
-                        <img class="slider-img" src="{{$item['gallery']}}">
-                        <div class="carousel-caption slider-text">
-                            <h3>{{$item['name']}}</h3>
-                            <p>{{$item['description']}}</p>
-                        </div>
+                        <a href="detail/{{$item['id']}}">
+                            <img class="slider-img" src="{{$item['gallery']}}">
+                            <div class="carousel-caption slider-text">
+                                <h3>{{$item['name']}}</h3>
+                                <p>{{$item['description']}}</p>
+                            </div>
+                        </a>
                     </div>
                 @endforeach
             </div>
@@ -36,10 +38,12 @@
             <h3>Trending Products</h3>
             @foreach($products as $item)
                 <div class="trending-item">
-                    <img class="trending-image" src="{{$item['gallery']}}">
-                    <div class="">
-                        <h3>{{$item['name']}}</h3>
-                    </div>
+                    <a href="detail/{{$item['id']}}">
+                        <img class="trending-image" src="{{$item['gallery']}}">
+                        <div class="">
+                            <h3>{{$item['name']}}</h3>
+                        </div>
+                    </a>
                 </div>
             @endforeach
         </div>

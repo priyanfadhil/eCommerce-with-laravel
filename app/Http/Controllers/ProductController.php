c83ff750286b3b7361a83234c3f5df4d3b5fffd8
priyanfadhil <priyanfadhil@gmail.com>
2020-12-21 23:18:37 +0700

    search selesai

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,4 +17,10 @@ class ProductController extends Controller
         $data = Product::find($id);
         return view('detail',['product'=>$data]);
     }
+    function search(Request $req)
+    {
+        $data = Product::
+        where('name', 'like', '%'. $req->input('query'). '%')->get();
+        return view('search',['products'=>$data]);
+    }
 }
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -46,5 +46,8 @@
     .detail-img{
         height: 200px;
     }
+    .search-box{
+        width: 500px !important;
+    }
 </style>
 </html>
